39c5ad2940e334e6aac6e2a902cc00e9fe507c6f
mingjun97 <my@lyric.today>
2022-05-19 03:42:40 -0700

    Add support for MojoConsole
    * Register self as a URL handler for schema `gccomh://`
    * Parse the command line arguments as mojoconsole parameter
    * Add a helper for send request to mojoconsole

--- GC-Command-Helper/App.xaml.cs
+++ GC-Command-Helper/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Newtonsoft.Json;
 
 namespace GC_Command_Helper
 {
@@ -13,5 +14,27 @@ namespace GC_Command_Helper
     /// </summary>
     public partial class App : Application
     {
+        private class MojoParam
+        {
+            public string d = "";
+            public string k2 = "";
+        }
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            MainWindow wnd = new MainWindow();
+            if (e.Args.Length == 1)
+            { 
+                Uri arg_url;
+                var arg = Uri.TryCreate(e.Args[0],UriKind.Absolute, out arg_url);
+                GlobalProps.MojoServer = arg_url.GetComponents(UriComponents.Fragment, UriFormat.Unescaped);
+                MojoParam mp = JsonConvert.DeserializeObject<MojoParam>(GlobalProps.MojoServer);
+                API.MojoApi.token = mp.k2;
+                API.MojoApi.api = mp.d + "/mojoplus/api";
+            }
+                
+            wnd.Show();
+        }
     }
+
+
 }
