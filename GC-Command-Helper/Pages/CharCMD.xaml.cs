ce260bd11359180b572dcf70b5e885426827575a
Core Swety <swety2003@outlook.com>
2022-05-19 12:45:45 +0800

    Updated

--- GC-Command-Helper/Pages/CharCMD.xaml.cs
+++ GC-Command-Helper/Pages/CharCMD.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +24,97 @@ namespace GC_Command_Helper.Pages
     /// </summary>
     public partial class CharCMD : Page
     {
+
+        CharCMDVM vm;
         public CharCMD()
         {
             InitializeComponent();
+        }
+
+
+
+        public class CharCMDVM:ObservableObject
+        {
+            public CharCMDVM()
+            {
+
+            }
+
+
+            public string ToCommand()
+            {
+                var cmd = $"/givechar {ID} {Value}";
+
+                GlobalProps.SetCMD(cmd);
+                return cmd;
+            }
+
+            private string _id;
+
+            public string ID
+            {
+                get { return _id; }
+                set { SetProperty(ref _id, value); ToCommand(); }
+            }
+
+            private double _value;
+
+            public double Value
+            {
+                get { return _value; }
+                set { SetProperty(ref _value, value);ToCommand(); }
+            }
+
+
+
+
+            private ObservableCollection<GlobalProps.SimpleItem> _items;
+
+            public ObservableCollection<GlobalProps.SimpleItem> Items
+            {
+                get { return _items; }
+                set { SetProperty(ref _items, value); }
+            }
+
+
+
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            vm = new CharCMDVM { Value=1 };
+
+            this.DataContext = vm;
+
+            LoadDataAsync();
+
+
+        }
+
+
+        public class Item
+        {
+            public string Name { get; set; }
+            public string ArgName { get; set; }
+            public bool Percent { get; set; }
+            public string Tip { get; set; }
+        }
+
+        public async Task LoadDataAsync()
+        {
+            var file = @"Resources\zh-cn\Avatar.txt";
+            var lines = File.ReadAllLines(file);
+            vm.Items = new ObservableCollection<GlobalProps.SimpleItem>();
+
+            foreach (var line in lines)
+            {
+                var item = line.Split(':');
+                vm.Items.Add(new GlobalProps.SimpleItem { Name = item[1], Id = item[0] });
+            }
+
+
+
+
         }
     }
 }
--- GC-Command-Helper/Pages/DataCMD.xaml.cs
+++ GC-Command-Helper/Pages/DataCMD.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +24,101 @@ namespace GC_Command_Helper.Pages
     /// </summary>
     public partial class DataCMD : Page
     {
+
+        CharCMDVM vm = new CharCMDVM();
         public DataCMD()
         {
             InitializeComponent();
+        }
+
+
+
+        public class CharCMDVM : ObservableObject
+        {
+            public CharCMDVM()
+            {
+
+            }
+
+
+            public string ToCommand()
+            {
+                var cmd = $"/setstats {ID} {Value}";
+
+                GlobalProps.SetCMD(cmd);
+                return cmd;
+            }
+
+            public string ToCommand1()
+            {
+                var cmd = $"/talent n 10";
+
+                GlobalProps.SetCMD(cmd);
+                return cmd;
+            }
+
+
+            private string _id;
+
+            public string ID
+            {
+                get { return _id; }
+                set { SetProperty(ref _id, value); ToCommand(); }
+            }
+
+            private double _value;
+
+            public double Value
+            {
+                get { return _value; }
+                set { SetProperty(ref _value, value); ToCommand(); }
+            }
+
+
+
+
+            private ObservableCollection<Item> _prop;
+
+            public ObservableCollection<Item> Prop
+            {
+                get { return _prop; }
+                set { SetProperty(ref _prop, value); }
+            }
+
+
+
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            vm = new CharCMDVM();
+
+            this.DataContext = vm;
+
+            LoadDataAsync();
+
+
+        }
+
+
+        public class Item
+        {
+            public string Name { get; set; }
+            public string ArgName { get; set; }
+            public bool Percent { get; set; }
+            public string Tip { get; set; }
+        }
+
+        public async Task LoadDataAsync()
+        {
+            var file = @"Resources\zh-cn\AvatarStats.json";
+            var lines = File.ReadAllText(file);
+            //vm.Prop = new ObservableCollection<Item>();
+            vm.Prop = JsonConvert.DeserializeObject<ObservableCollection<Item>>(lines);
+
+
+
+
         }
     }
 }
